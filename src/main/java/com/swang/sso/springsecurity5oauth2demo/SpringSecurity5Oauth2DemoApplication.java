15f21b7cad6e3f6f2275df827c5f59c6d4d86ed6
swang <mingisme@126.com>
2019-10-11 10:38:37 +0800

    init

---
+++ src/main/java/com/swang/sso/springsecurity5oauth2demo/SpringSecurity5Oauth2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.swang.sso.springsecurity5oauth2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity5Oauth2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity5Oauth2DemoApplication.class, args);
+	}
+
+}
